91002c01d7396e65a19ceacb38e787cfb5735429
Rahul-Gautam-7 <rahulgautam7r@gmail.com>
2024-06-03 18:43:20 +0530

    diamond pattern

---
+++ diamondpattern.java
@@ -0,0 +1,30 @@
+public class diamondpattern {
+    public static void main(String[] args) {
+        for(int i=0;i<5;i++)
+        {
+            for(int j=5-i;j>1;j--)
+            {
+                System.out.print(" ");
+            }
+            for(int j=0;j<=i;j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+     
+        for(int i=5;i>0;i--)
+        {
+            for(int j=1;j<=5-i;j++)
+            {
+                System.out.print(" ");
+            }
+            for(int j=0;j<i;j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    
+}
