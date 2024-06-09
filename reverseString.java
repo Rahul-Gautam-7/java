128741407492c4f34a23e8fa2def8c3153f95f10
Rahul-Gautam-7 <rahulgautam7r@gmail.com>
2024-06-09 17:56:03 +0530

    reverse string

---
+++ reverseString.java
@@ -0,0 +1,11 @@
+public class reverseString {
+
+
+    public static void main(String[] args) {
+    
+        String str = "hello this is a string reverse";
+        StringBuilder sb = new StringBuilder(str);
+        System.out.println(sb.reverse());
+    }
+    
+}
