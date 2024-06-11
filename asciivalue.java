51ec407167a8bac2fd6715b502ac4acc42c28005
Rahul-Gautam-7 <rahulgautam7r@gmail.com>
2024-06-11 18:20:49 +0530

    ascii value

---
+++ asciivalue.java
@@ -0,0 +1,11 @@
+public class asciivalue{
+    public static void main(String[] args) {
+        char ch = 'a';
+        char ch1 = 'A';
+
+        int n1 = ch;
+        int n2 = ch1;
+        System.out.println(ch+" ascii value is "+n1);
+        System.out.println(ch1+" ascii value is "+n2);
+    }
+}
